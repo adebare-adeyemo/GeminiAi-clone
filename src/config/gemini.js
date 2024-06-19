cf89009fbea6a069e16197cc2707e0c82f042e47
Adebare Adeyemo <113603612+adebare-adeyemo@users.noreply.github.com>
2024-06-19 17:26:46 +0100

    Update gemini.js
    updated the ai api to gemini 1.5 pro

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,44 +1,48 @@
-
-  /* Install the Generative AI SDK
+/*
+ * Install the Generative AI SDK
  *
  * $ npm install @google/generative-ai
  *
  * See the getting started guide for more information
  * https://ai.google.dev/gemini-api/docs/get-started/node
  */
-// removed the intial const and required 
+
 import {
     GoogleGenerativeAI,
     HarmCategory,
     HarmBlockThreshold,
-} from "@google/generative-ai";
-
-const apiKey = "AIzaSyDoJMUZzF0S-1qlQEnq6ozIuV9fOkhCqk8"; // Replace with your actual API key
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
-});
-
-const generationConfig = {
-    temperature: 0.9,
-    topP: 1,
-    topK: 0,
-    maxOutputTokens: 2048,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDoJMUZzF0S-1qlQEnq6ozIuV9fOkhCqk8"; // Replace with your actual API key
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
     responseMimeType: "text/plain",
-};
-// add prompt 
-async function run(prompt) {
+  };
+  
+  async function run(prompt) {
     const chatSession = model.startChat({
-        generationConfig,
-        // safetySettings: Adjust safety settings
-        // See https://ai.google.dev/gemini-api/docs/safety-settings
-        history: [],
+      generationConfig,
+      // safetySettings: Adjust safety settings
+      // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [],
     });
-// added prompt here 
+  
     const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
     return result.response.text();
-}
-
+  }
+  
+//   // Example usage
+//   const prompt = "INSERT_INPUT_HERE"; // Replace with actual input
+//   run(prompt); // Call run with the prompt
+  
 export default run;
